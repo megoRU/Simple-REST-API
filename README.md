1be76c0b39d5fe45a325a8df73e3a9d07a7a1a55
mego <savin.yura2014@gmail.com>
2021-01-02 20:55:40 +0300

    edit domain

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 https://app-mego.herokuapp.com/games/dice
 
 ## Usage Documentation
-[![gitbook](https://github.com/megoRU/DiscordBot/blob/master/images/gitbook.jpg?raw=true)](https://discord.megolox.ru/api/game-of-dice-rest-api)
+[![gitbook](https://github.com/megoRU/DiscordBot/blob/master/images/gitbook.jpg?raw=true)](https://discord.megoru.ru/api/game-of-dice-rest-api)
--- src/main/java/com/simple/rest/api/controller/Rest.java
+++ src/main/java/com/simple/rest/api/controller/Rest.java
@@ -12,193 +12,192 @@ import java.util.Random;
 @RequestMapping(value = "games/dice", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 public class Rest {
 
-    private final Random random = new Random();
-    private String first = null;
-    private String second = null;
+  private final Random random = new Random();
+  private String first = null;
+  private String second = null;
 
-    @GetMapping
-    private Json list() {
-        random();
-        return new Json(first, second, choiceOfSides());
+  @GetMapping
+  private Json list() {
+    random();
+    return new Json(first, second, choiceOfSides());
+  }
+
+  private void random() {
+    first = String.valueOf(random.nextInt(6) + 1);
+    second = String.valueOf(random.nextInt(6) + 1);
+  }
+
+  private String choiceOfSides() {
+
+    //1
+    if (first.equals("1")) {
+
+      if (second.equals("1")) {
+        return "https://megoru.ru/disImages/1-1.png";
+      }
+
+      if (second.equals("2")) {
+        return "https://megoru.ru/disImages/1-2.png";
+      }
+
+      if (second.equals("3")) {
+        return "https://megoru.ru/disImages/1-3.png";
+      }
+
+      if (second.equals("4")) {
+        return "https://megoru.ru/disImages/1-4.png";
+      }
+
+      if (second.equals("5")) {
+        return "https://megoru.ru/disImages/1-5.png";
+      }
+
+      if (second.equals("6")) {
+        return "https://megoru.ru/disImages/1-6.png";
+      }
     }
 
-    private void random() {
-       first = String.valueOf(random.nextInt(6) + 1);
-       second = String.valueOf(random.nextInt(6) + 1);
+    //2
+
+    if (first.equals("2")) {
+
+      if (second.equals("1")) {
+        return "https://megoru.ru/disImages/2-1.png";
+      }
+
+      if (second.equals("2")) {
+        return "https://megoru.ru/disImages/2-2.png";
+      }
+
+      if (second.equals("3")) {
+        return "https://megoru.ru/disImages/2-3.png";
+      }
+
+      if (second.equals("4")) {
+        return "https://megoru.ru/disImages/2-4.png";
+      }
+
+      if (second.equals("5")) {
+        return "https://megoru.ru/disImages/2-5.png";
+      }
+
+      if (second.equals("6")) {
+        return "https://megoru.ru/disImages/2-6.png";
+      }
     }
 
-    private String choiceOfSides() {
+    //3
+    if (first.equals("3")) {
 
-        //1
-        if (first.equals("1")) {
+      if (second.equals("1")) {
+        return "https://megoru.ru/disImages/3-1.png";
+      }
 
-            if (second.equals("1")) {
-                return "https://megolox.ru/disImages/1-1.png";
-            }
+      if (second.equals("2")) {
+        return "https://megoru.ru/disImages/3-2.png";
+      }
 
-            if (second.equals("2")) {
-                return "https://megolox.ru/disImages/1-2.png";
-            }
+      if (second.equals("3")) {
+        return "https://megoru.ru/disImages/3-3.png";
+      }
 
-            if (second.equals("3")) {
-                return "https://megolox.ru/disImages/1-3.png";
-            }
+      if (second.equals("4")) {
+        return "https://megoru.ru/disImages/3-4.png";
+      }
 
-            if (second.equals("4")) {
-                return "https://megolox.ru/disImages/1-4.png";
-            }
+      if (second.equals("5")) {
+        return "https://megoru.ru/disImages/3-5.png";
+      }
 
-            if (second.equals("5")) {
-                return "https://megolox.ru/disImages/1-5.png";
-            }
-
-            if (second.equals("6")) {
-                return "https://megolox.ru/disImages/1-6.png";
-            }
-        }
-
-        //2
-
-        if (first.equals("2")) {
-
-            if (second.equals("1")) {
-                return "https://megolox.ru/disImages/2-1.png";
-            }
-
-            if (second.equals("2")) {
-                return "https://megolox.ru/disImages/2-2.png";
-            }
-
-            if (second.equals("3")) {
-                return "https://megolox.ru/disImages/2-3.png";
-            }
-
-            if (second.equals("4")) {
-                return "https://megolox.ru/disImages/2-4.png";
-            }
-
-            if (second.equals("5")) {
-                return "https://megolox.ru/disImages/2-5.png";
-            }
-
-            if (second.equals("6")) {
-                return "https://megolox.ru/disImages/2-6.png";
-            }
-        }
-
-        //3
-        if (first.equals("3")) {
-
-
-            if (second.equals("1")) {
-                return "https://megolox.ru/disImages/3-1.png";
-            }
-
-            if (second.equals("2")) {
-                return "https://megolox.ru/disImages/3-2.png";
-            }
-
-            if (second.equals("3")) {
-                return "https://megolox.ru/disImages/3-3.png";
-            }
-
-            if (second.equals("4")) {
-                return "https://megolox.ru/disImages/3-4.png";
-            }
-
-            if (second.equals("5")) {
-                return "https://megolox.ru/disImages/3-5.png";
-            }
-
-            if (second.equals("6")) {
-                return "https://megolox.ru/disImages/3-6.png";
-            }
-        }
-
-        //4
-        if (first.equals("4")) {
-
-            if (second.equals("1")) {
-                return "https://megolox.ru/disImages/4-1.png";
-            }
-
-            if (second.equals("2")) {
-                return "https://megolox.ru/disImages/4-2.png";
-            }
-
-            if (second.equals("3")) {
-                return "https://megolox.ru/disImages/4-3.png";
-            }
-
-            if (second.equals("4")) {
-                return "https://megolox.ru/disImages/4-4.png";
-            }
-
-            if (second.equals("5")) {
-                return "https://megolox.ru/disImages/4-5.png";
-            }
-
-            if (second.equals("6")) {
-                return "https://megolox.ru/disImages/4-6.png";
-            }
-        }
-
-        //
-        if (first.equals("5")) {
-
-            if (second.equals("1")) {
-                return "https://megolox.ru/disImages/5-1.png";
-            }
-
-            if (second.equals("2")) {
-                return "https://megolox.ru/disImages/5-2.png";
-            }
-
-            if (second.equals("3")) {
-                return "https://megolox.ru/disImages/5-3.png";
-            }
-
-            if (second.equals("4")) {
-                return "https://megolox.ru/disImages/5-4.png";
-            }
-
-            if (second.equals("5")) {
-                return "https://megolox.ru/disImages/5-5.png";
-            }
-
-            if (second.equals("6")) {
-                return "https://megolox.ru/disImages/5-6.png";
-            }
-        }
-
-        //
-        if (first.equals("6")) {
-
-            if (second.equals("1")) {
-                return "https://megolox.ru/disImages/6-1.png";
-            }
-
-            if (second.equals("2")) {
-                return "https://megolox.ru/disImages/6-2.png";
-            }
-
-            if (second.equals("3")) {
-                return "https://megolox.ru/disImages/6-3.png";
-            }
-
-            if (second.equals("4")) {
-                return "https://megolox.ru/disImages/6-4.png";
-            }
-
-            if (second.equals("5")) {
-                return "https://megolox.ru/disImages/6-5.png";
-            }
-
-            if (second.equals("6")) {
-                return "https://megolox.ru/disImages/6-6.png";
-            }
-        }
-
-        return "https://megolox.ru/dice.png";
+      if (second.equals("6")) {
+        return "https://megoru.ru/disImages/3-6.png";
+      }
     }
+
+    //4
+    if (first.equals("4")) {
+
+      if (second.equals("1")) {
+        return "https://megoru.ru/disImages/4-1.png";
+      }
+
+      if (second.equals("2")) {
+        return "https://megoru.ru/disImages/4-2.png";
+      }
+
+      if (second.equals("3")) {
+        return "https://megoru.ru/disImages/4-3.png";
+      }
+
+      if (second.equals("4")) {
+        return "https://megoru.ru/disImages/4-4.png";
+      }
+
+      if (second.equals("5")) {
+        return "https://megoru.ru/disImages/4-5.png";
+      }
+
+      if (second.equals("6")) {
+        return "https://megoru.ru/disImages/4-6.png";
+      }
+    }
+
+    //
+    if (first.equals("5")) {
+
+      if (second.equals("1")) {
+        return "https://megoru.ru/disImages/5-1.png";
+      }
+
+      if (second.equals("2")) {
+        return "https://megoru.ru/disImages/5-2.png";
+      }
+
+      if (second.equals("3")) {
+        return "https://megoru.ru/disImages/5-3.png";
+      }
+
+      if (second.equals("4")) {
+        return "https://megoru.ru/disImages/5-4.png";
+      }
+
+      if (second.equals("5")) {
+        return "https://megoru.ru/disImages/5-5.png";
+      }
+
+      if (second.equals("6")) {
+        return "https://megoru.ru/disImages/5-6.png";
+      }
+    }
+
+    //
+    if (first.equals("6")) {
+
+      if (second.equals("1")) {
+        return "https://megoru.ru/disImages/6-1.png";
+      }
+
+      if (second.equals("2")) {
+        return "https://megoru.ru/disImages/6-2.png";
+      }
+
+      if (second.equals("3")) {
+        return "https://megoru.ru/disImages/6-3.png";
+      }
+
+      if (second.equals("4")) {
+        return "https://megoru.ru/disImages/6-4.png";
+      }
+
+      if (second.equals("5")) {
+        return "https://megoru.ru/disImages/6-5.png";
+      }
+
+      if (second.equals("6")) {
+        return "https://megoru.ru/disImages/6-6.png";
+      }
+    }
+
+    return "https://megoru.ru/dice.png";
+  }
 }
