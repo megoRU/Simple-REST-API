50b366b81b6e035d6dc8d75a38b432bfda4613de
mego <savin.yura2014@gmail.com>
2021-03-16 18:30:09 +0300

    Some fix

--- src/main/java/com/simple/rest/api/controller/Json.java
+++ src/main/java/com/simple/rest/api/controller/Json.java
@@ -2,25 +2,25 @@ package com.simple.rest.api.controller;
 
 public class Json {
 
-    private Integer firstNumber;
-    private Integer secondNumber;
-    private String url;
+  private final Integer firstNumber;
+  private final Integer secondNumber;
+  private final String url;
 
-    public Json(Integer firstNumber, Integer secondNumber, String url) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-        this.url = url;
-    }
+  public Json(Integer firstNumber, Integer secondNumber, String url) {
+    this.firstNumber = firstNumber;
+    this.secondNumber = secondNumber;
+    this.url = url;
+  }
 
-    public Integer getFirstNumber() {
-        return firstNumber;
-    }
+  public Integer getFirstNumber() {
+    return firstNumber;
+  }
 
-    public Integer getSecondNumber() {
-        return secondNumber;
-    }
+  public Integer getSecondNumber() {
+    return secondNumber;
+  }
 
-    public String getUrl() {
-        return url;
-    }
+  public String getUrl() {
+    return url;
+  }
 }
--- src/main/java/com/simple/rest/api/controller/Rest.java
+++ src/main/java/com/simple/rest/api/controller/Rest.java
@@ -2,28 +2,25 @@ package com.simple.rest.api.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
 @RestController
-@RequestMapping(value = "games/dice", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 public class Rest {
 
   private final Random random = new Random();
-  private Integer first = null;
-  private Integer second = null;
 
-  @GetMapping
+  @GetMapping(value = "games/dice", produces = MediaType.APPLICATION_JSON_VALUE)
   private Json list() {
+    int first;
+    int second;
     return new Json(first = random.nextInt(6) + 1,
         second = random.nextInt(6) + 1,
-        choiceOfSides());
+        choiceOfSides(first, second));
   }
 
-  private String choiceOfSides() {
+  private String choiceOfSides(int first, int second) {
     return "https://megoru.ru/disImages/" + first + "-" + second + ".png";
   }
 }
